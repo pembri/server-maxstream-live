6ab5bb871da25a25523c03687884d4e2cbf10c84
pembri <pembriahmad@gmail.com>
2026-06-21 11:54:15 +0700

    Update proxy.py

--- api/proxy.py
+++ api/proxy.py
@@ -234,7 +234,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-Type", "application/vnd.apple.mpegurl")
         self.send_header("Access-Control-Allow-Origin", "*")
-        self.send_header("Cache-Control", "no-cache")
+        self.send_header("Cache-Control", "no-store, max-age=0, must-revalidate")
         self.end_headers()
         self.wfile.write(body.encode())
 
